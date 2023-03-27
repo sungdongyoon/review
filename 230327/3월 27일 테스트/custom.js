de99462e517c62ec54417a7e7d613e4bc77326ba
sungdongyoon <104995424+sungdongyoon@users.noreply.github.com>
2023-03-27 18:23:54 +0900

    Add files via upload

---
+++ 230327/3월 27일 테스트/custom.js
@@ -0,0 +1,12 @@
+$(function () {
+  /* Slick Slider */
+  $(".project_slick").slick({
+    dots: true,
+    infinite: true,
+    speed: 200,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  });
+});
