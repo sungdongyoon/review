508ba086528e0e5cbd0158db704720af2514c69c
sungdongyoon <104995424+sungdongyoon@users.noreply.github.com>
2023-03-31 16:22:27 +0900

    Add files via upload

---
+++ 230330/script.js
@@ -0,0 +1,4 @@
+let menu = ["김치찌개", "칼국수", "햄버거", "김밥", "파스타"];
+let menuNum = Math.floor(Math.random() * menu.length);
+let result = menu[menuNum];
+document.write(result);
--- 230330/투두리스트/todo_list.js
+++ 230330/투두리스트/todo_list.js
@@ -2,13 +2,63 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-form.addEventListener("submit", function (event) {
+let todos = [];
+const save = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const delItem = (event) => {
+  const target = event.target.parentElement;
+  todos = todos.filter((todo) => todo.id != target.id);
+  save();
+  target.remove();
+};
+
+const addItem = (todo) => {
+  if (todo.text !== "") {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const span = document.createElement("span");
+
+    span.innerText = todo.text;
+    button.innerText = "삭제";
+    button.addEventListener("click", delItem);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    ul.appendChild(li);
+    li.id = todo.id;
+  }
+};
+
+const handler = (event) => {
   event.preventDefault();
 
-  if (input.value !== "") {
-    const li = document.createElement("li");
-    li.innerText = input.value;
-    ul.appendChild(li);
-    input.value = "";
+  const todo = {
+    id: Date.now(),
+    text: input.value,
+  };
+
+  todos.push(todo);
+  addItem(todo);
+  save();
+  input.value = "";
+};
+
+const init = () => {
+  // const userTodos = localStorage.getItem("todos");
+  // console.log(userTodos);
+  const userTodos = JSON.parse(localStorage.getItem("todos"));
+  if (userTodos) {
+    userTodos.forEach((todo) => {
+      addItem(todo);
+    });
   }
-});
+};
+
+init();
+form.addEventListener("submit", handler);
+
+// localStorage.setItem("Hello", "World");
+// const myData = localStorage.getItem("Hello");
+// console.log(myData);
